3ca2bef7463418ef5f688abdea7f3b2c0ea9feaf
LeeKyuHyuk <LeeKyuHyuk@users.noreply.github.com>
2022-05-13 02:36:58 +0000

    Deploying to gh-pages from @ LeeKyuHyuk/resume@7194004921b40e76213bf05d550d09f3201f6121 🚀

---
+++ _next/static/jnuOSXgh27PGOoteSGbA8/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/526-45e551f7b4508102.js","static/chunks/pages/index-42a22cad1ced70f6.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
